920813bd2a0438a4ddc6915d9b5e63871a80b251
BISIAUX Alexandre <bisiaux.alexandre@gmail.com>
2016-07-08 17:51:58 +0200

    Update updater.py

--- Contents/Code/updater.py
+++ Contents/Code/updater.py
@@ -1,5 +1,6 @@
 ################################################################################
-GITHUB_REPOSITORY = 'sharkone/BitTorrent.bundle'
+GITHUB_REPOSITORY = 'abisiaux/BitTorrent.bundle'
+LAST_VERSION = 'master'
 
 ################################################################################
 def get_latest_version():
@@ -12,37 +13,30 @@ def get_latest_version():
 
 ################################################################################
 def update_available():
-    latest_version_str = get_latest_version()
-    if latest_version_str:
-        latest_version  = map(int, latest_version_str.split('.'))
-        current_version = map(int, SharedCodeService.common.VERSION.split('.'))
-        return (latest_version[0] > current_version[0] or latest_version[1] > current_version[1] or latest_version[2] > current_version[2], latest_version_str)
-    return (False, None)
+    return (True, None)
 
 ################################################################################
 @route(SharedCodeService.common.PREFIX + '/update')
 def update():
     try:
-        latest_version = get_latest_version()
-        if latest_version:
-            zip_data = Archive.ZipFromURL('https://github.com/{0}/archive/{1}.zip'.format(GITHUB_REPOSITORY, latest_version))
+        zip_data = Archive.ZipFromURL('https://github.com/{0}/archive/{1}.zip'.format(GITHUB_REPOSITORY, LAST_VERSION))
 
-            SharedCodeService.scrapmagnet.stop()
+        SharedCodeService.scrapmagnet.stop()
 
-            for name in zip_data.Names():
-                data    = zip_data[name]
-                parts   = name.split('/')
-                shifted = Core.storage.join_path(*parts[1:])
-                full    = Core.storage.join_path(Core.bundle_path, shifted)
+        for name in zip_data.Names():
+            data    = zip_data[name]
+            parts   = name.split('/')
+            shifted = Core.storage.join_path(*parts[1:])
+            full    = Core.storage.join_path(Core.bundle_path, shifted)
 
-                if '/.' in name:
-                    continue
+            if '/.' in name:
+                continue
 
-                if name.endswith('/'):
-                    Core.storage.ensure_dirs(full)
-                else:
-                    Core.storage.save(full, data)
-            del zip_data
-            return ObjectContainer(header='Update successful', message='Channel updated to version {0}'.format(latest_version))
+            if name.endswith('/'):
+                Core.storage.ensure_dirs(full)
+            else:
+                Core.storage.save(full, data)
+        del zip_data
+        return ObjectContainer(header='Update successful', message='Channel updated to version {0}'.format(latest_version))
     except Exception as exception:
         return ObjectContainer(header='Update failed', message=str(exception))
